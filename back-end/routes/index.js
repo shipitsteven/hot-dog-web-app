8bd651265dd1acb3381d7170f40a4d5827b207b2
swang93 <swang93@uw.edu>
2021-03-28 21:29:14 -0700

    added fetch in landing page to warm up server

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../database/connection');
+
+const query = {
+  dbWarmUp: 'SELECT * FROM Cart;',
+};
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function (req, res) {
+  await db.promise().execute(query.dbWarmUp);
+  res.send('Server ready.');
 });
 
 module.exports = router;
